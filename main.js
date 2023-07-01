b0a8ff6b27f8a6efb86a56623b03865745ce80c8
Bobbianss <s287295@studenti.polito.it>
2023-07-01 18:14:32 +0200

    new tech

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
-import './style.css'
 
+import * as THREE from "three";
+//import { MindARThree } from 'mind-ar/dist/mindar-image-three.prod';
+//import { loadGLTF, loadAudio, loadVideo, loadTexture } from './public/libs/loader.js';
+//import mindar from "./public/pages/mindar.js"
+import './style.css'
