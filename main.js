d4a5b0f768f56c884a71e826ef907390d9fca468
Bobbianss <s287295@studenti.polito.it>
2023-11-11 18:43:43 +0100

    add libs

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+import * as THREE from "three/build/three.module.js";
+import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader.js";
+
 
 import './style.css'
 //import './public/pages/webxr.js'
